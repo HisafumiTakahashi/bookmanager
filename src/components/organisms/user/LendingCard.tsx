80da1acecb3422c3bba9f9f633a0c9ea46026631
hisa <octn.wps.0108@gmail.com>
2024-02-29 10:24:08 +0900

    返却処理追加

---
+++ src/components/organisms/user/LendingCard.tsx
@@ -0,0 +1,47 @@
+/*
+貸出中の書籍情報項目を実装。
+*/
+import { memo, VFC } from "react";
+import { Box, Stack, Text } from "@chakra-ui/react";
+
+type Props = {
+  isbn: number;
+  title: string;
+  isLending: boolean;
+  lender: string;
+  lendingYMD: Date;
+};
+
+export const LendingCard: VFC<Props> = memo((props) => {
+  const { title, lender, lendingYMD } = props;
+
+  let LendingYMD = lendingYMD.toString();
+  LendingYMD =
+    LendingYMD.substring(0, 4) +
+    "/" +
+    LendingYMD.substring(4, 6) +
+    "/" +
+    LendingYMD.substring(6, 8);
+
+  return (
+    <>
+      <Box w="500px" h="70px" shadow="md" p={4}>
+        <Stack textAlign="center">
+          <Text fontSize="md">{title}</Text>
+        </Stack>
+      </Box>
+
+      <Box w="250px" h="70px" shadow="md" p={4}>
+        <Stack textAlign="center">
+          <Text fontSize="md">{lender}</Text>
+        </Stack>
+      </Box>
+
+      <Box w="250px" h="70px" shadow="md" p={4}>
+        <Stack textAlign="center">
+          <Text fontSize="md">{LendingYMD}</Text>
+        </Stack>
+      </Box>
+    </>
+  );
+});
